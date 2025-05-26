0d4b1e85f36983ba5bf8a5d82c9168c4a1538029
ketarubot <82302028+ketarubot@users.noreply.github.com>
2025-05-26 15:03:29 +0900

    FastAPI 2

--- back/todos/src/main.py
+++ back/todos/src/main.py
@@ -1,47 +1,25 @@
+# FastAPI import
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-class CreateToDoRequest(BaseModel):
-    id: int
-    contents: str
-    is_done: bool
-
-
+# FastAPI instance 생성. 
 app = FastAPI()
 
-todo_data = {
-    1: {
-        'id': 1,
-        'contents': 'FastAPI 1',
-        'is_done': True,
-    },
-    2: {
-        'id': 2,
-        'contents': 'FastAPI 2',
-        'is_done': False,
-    },
-    3: {
-        'id': 3,
-        'contents': 'FastAPI 3',
-        'is_done': False,
-    },
-}
-@app.get('/')
-def heath_check_handler():
-    return {'ping':'pong'}
+# Path 오퍼레이션 생성. Path는 도메인명을 제외하고 / 로 시작하는 URL 부분
+# 만약 url이 https://example.com/items/foo 라면 path는 /items/foo 
+# Operation은 GET, POST, PUT/PATCH, DELETE등의 HTTP 메소드임. 
+@app.get("/", summary="간단한 API", tags=["simple"])
+def root():
+    '''
+    간단한 API
+    - 인자값 1은 ~
+    - 인자값 2는 ~
+    '''
+    return {"message": "Hello World"}
 
-@app.get('/todos')
-def get_todos_handler(order: str | None = None):
-    ret = list(todo_data.values())
-    if order and order == 'DESC':
-        return ret[::-1]
-    return ret
-
-@app.post('/todos')
-def create_todo_handler(request: CreateToDoRequest):
-    todo_data[request.id] = request.dict()
-    return todo_data[request.id]
-
-@app.get('/todos/{todo_id}')
-def get_todo_handler(todo_id: int):
-    return todo_data.get(todo_id, {})
+@app.get('/greet/{name}', summary='헬로', tags=['simple'])
+def greet(name: str):
+    '''
+    헬로
+    - 이름 받아서 Hello, {이름}! 출력
+    '''
+    return f'Hello, {name}!'
